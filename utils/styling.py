5a4bd13c9c8d4cbf59254b5a414b494121e05b0d
rakshithahj <rakshithahj48@gmail.com>
2026-06-04 16:04:15 +0530

    Update styling.py

--- utils/styling.py
+++ utils/styling.py
@@ -1,1 +1,25 @@
+import streamlit as st
 
+def load_css():
+
+    st.markdown("""
+    <style>
+
+    .metric-card{
+        background:#1e293b;
+        padding:20px;
+        border-radius:15px;
+        text-align:center;
+        box-shadow:0px 4px 12px rgba(0,0,0,0.2);
+    }
+
+    .insight-box{
+        background:#16213e;
+        padding:15px;
+        border-radius:12px;
+        margin-bottom:10px;
+    }
+
+    </style>
+    """,
+    unsafe_allow_html=True)
