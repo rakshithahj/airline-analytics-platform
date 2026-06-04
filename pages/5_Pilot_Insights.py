be9d5276353bdb1aec8d91bb81e287f10857ef5c
rakshithahj <rakshithahj48@gmail.com>
2026-06-04 15:55:59 +0530

    Update 5_Pilot_Insights.py

--- pages/5_Pilot_Insights.py
+++ pages/5_Pilot_Insights.py
@@ -1,1 +1,15 @@
-
+pilot_stats = (
+    df.groupby("Pilot Name")
+      .agg({
+          "Flight Status":"count"
+      })
+)
+Pilot vs Flight Status Heatmap
+df["Departure Date"] = pd.to_datetime(
+    df["Departure Date"]
+)
+fig = px.line(
+    monthly_data,
+    x="Month",
+    y="Flights"
+)
