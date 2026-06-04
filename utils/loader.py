e366d2ffabf964a92d83c0109f8c0541191b0d5b
rakshithahj <rakshithahj48@gmail.com>
2026-06-04 16:03:25 +0530

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import streamlit as st
 
+@st.cache_data
+def load_data():
+
+    df = pd.read_csv(
+        "data/Airline Dataset Updated - v2.csv"
+    )
+
+    df["Departure Date"] = pd.to_datetime(
+        df["Departure Date"],
+        errors="coerce"
+    )
+
+    return df
