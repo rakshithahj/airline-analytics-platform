25198790a56c2ce6f54cf910875f5f7f44b8dcbe
rakshithahj <rakshithahj48@gmail.com>
2026-06-04 15:51:16 +0530

    Update 2_Passenger_Analytics.py

--- pages/2_Passenger_Analytics.py
+++ pages/2_Passenger_Analytics.py
@@ -1,1 +1,16 @@
-
+fig = px.histogram(
+    df,
+    x="Age",
+    nbins=25,
+    marginal="box"
+)
+fig = px.sunburst(
+    df,
+    path=["Gender","Flight Status"]
+)
+0-18
+19-35
+36-50
+51-65
+65+
+Age Group × Flight Status
