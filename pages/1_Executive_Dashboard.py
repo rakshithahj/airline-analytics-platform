d642778d55373adce526c2d81bf30a5c107273d4
rakshithahj <rakshithahj48@gmail.com>
2026-06-04 15:48:57 +0530

    Update 1_Executive_Dashboard.py

--- pages/1_Executive_Dashboard.py
+++ pages/1_Executive_Dashboard.py
@@ -1,1 +1,10 @@
-
+Total Passengers
+Total Flights
+On-Time %
+Delayed %
+Cancelled %
+Unique Airports
+Unique Countries
+Unique Pilots
+st.metric("Passengers", f"{len(df):,}")
+st.metric("On-Time Rate", f"{on_time_rate:.2f}%")
