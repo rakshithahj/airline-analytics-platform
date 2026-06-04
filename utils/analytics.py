1167b82b21dacd6b833de66a5fe536ae6cfcffc1
rakshithahj <rakshithahj48@gmail.com>
2026-06-04 16:04:43 +0530

    Update analytics.py

--- utils/analytics.py
+++ utils/analytics.py
@@ -1,1 +1,40 @@
+import pandas as pd
 
+def get_kpis(df):
+
+    return {
+
+        "Passengers": len(df),
+
+        "Airports":
+        df["Airport Name"].nunique(),
+
+        "Countries":
+        df["Country Name"].nunique(),
+
+        "Pilots":
+        df["Pilot Name"].nunique(),
+
+        "Average Age":
+        round(df["Age"].mean(), 1)
+    }
+
+
+def top_airports(df, n=10):
+
+    return (
+        df["Airport Name"]
+        .value_counts()
+        .head(n)
+        .reset_index()
+    )
+
+
+def top_pilots(df, n=10):
+
+    return (
+        df["Pilot Name"]
+        .value_counts()
+        .head(n)
+        .reset_index()
+    )
